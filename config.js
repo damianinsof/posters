cc260e0c620a86706b074707107fe4c43505e656
DamianJ <sabybna@gmail.com>
2024-04-27 11:35:52 -0300

    inicial

---
+++ config.js
@@ -0,0 +1,8 @@
+const mode = "PROD"
+const URL_API = (mode =="PROD")
+        ? 
+         "posters-backend-production.up.railway.app"
+         :
+         "http://localhost:8080"
+         
+export {URL_API}
